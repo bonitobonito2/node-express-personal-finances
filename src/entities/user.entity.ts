3967a2217daab31b8c20f021cc21d52df90c0f38
bonitobonito2 <zaal.tsagareli.1@btu.edu.ge>
2023-03-16 19:43:43 +0400

    added relationship to users entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+import { Category } from "./category.entity";
 
 @Entity("user")
 export class User {
@@ -10,4 +11,7 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany(() => Category, (category) => category.user)
+  category: Category[];
 }
