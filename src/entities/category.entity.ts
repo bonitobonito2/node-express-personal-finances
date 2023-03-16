23ae0677a0cc8d12e158ef80502051992115a2a0
bonitobonito2 <zaal.tsagareli.1@btu.edu.ge>
2023-03-16 19:43:19 +0400

    category entity

---
+++ src/entities/category.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  OneToMany,
+} from "typeorm";
+import { User } from "./user.entity";
+import { Records } from "./records.entity";
+
+@Entity("category")
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  categoryName: string;
+
+  @ManyToOne(() => User, (user) => user.category)
+  user: User;
+
+  @OneToMany(() => Records, (category) => category.category)
+  record: Records[];
+}
