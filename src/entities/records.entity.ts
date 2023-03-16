3414123e9b91de73c057ed8f0b4f6ba11fc7d069
bonitobonito2 <zaal.tsagareli.1@btu.edu.ge>
2023-03-16 19:43:29 +0400

    records entity

---
+++ src/entities/records.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
+import { Category } from "./category.entity";
+import { RecordTypeEnum } from "../enums/records.enum";
+
+@Entity("records")
+export class Records {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  categoryName: string;
+
+  @Column()
+  descriotion: string;
+
+  @Column()
+  price: string;
+
+  @Column()
+  process: string;
+
+  @Column({ enum: RecordTypeEnum })
+  type: RecordTypeEnum;
+
+  @ManyToOne(() => Category, (user) => user.record)
+  category: Category[];
+}
