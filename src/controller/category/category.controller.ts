39253eec04b79d4cc1cf2dc4f204c607128afc64
bonitobonito2 <zaal.tsagareli.1@btu.edu.ge>
2023-03-18 17:59:35 +0400

    logic to delete category and save its records in default category

--- src/controller/category/category.controller.ts
+++ src/controller/category/category.controller.ts
@@ -1,6 +1,7 @@
 import { RequestHandler, Router } from "express";
 import { AuthService } from "../../service/auth.service";
 import { CategoryService } from "../../service/category.service";
+import { RecordService } from "../../service/record.service";
 
 export const createCategory: RequestHandler = async (
   request,
@@ -18,7 +19,7 @@ export const createCategory: RequestHandler = async (
     const userExsists = await authService.getUser(userName);
 
     if (userExsists && userExsists.password == password) {
-      const createCategory = await categoryService.createCategory(
+      const createCategory = await categoryService.createCategoryAndCheck(
         categoryName,
         userExsists
       );
@@ -72,6 +73,74 @@ export const changeCategoryName: RequestHandler = async (
       }
     }
     return response.status(401).json("your userName or password is incorrect");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteCategory: RequestHandler = async (
+  request,
+  response,
+  next
+) => {
+  const categoryId = parseInt(request.params["id"]);
+  const userName = request.body["userName"];
+  const password = request.body["password"];
+
+  const authService = new AuthService();
+  const categoryService = new CategoryService();
+  const recordService = new RecordService();
+  try {
+    const userExsists = await authService.getUser(userName);
+    if (userExsists && userExsists.password == password) {
+      const userHasCategory = await categoryService.userHasCategory(
+        categoryId,
+        userExsists
+      );
+
+      if (typeof userHasCategory !== "boolean") {
+        //getting records of this category
+        const records = await recordService.getRecordsByCategory(
+          userHasCategory
+        );
+
+        //deleting categories and it's records
+        await categoryService.deleteCategory(userHasCategory, records);
+
+        //checking if user has a default category
+        const userHasDefaultCategory =
+          await categoryService.userHasCategoryByName("default", userExsists);
+        if (typeof userHasDefaultCategory !== "boolean") {
+          //if user has a default category, moving old records in default
+          await recordService.moveRecordsToDefaultCategory(
+            records,
+            userHasDefaultCategory
+          );
+          return response.json(
+            `deleted ${userHasCategory.categoryName} and moved it's records in default category`
+          );
+        } else {
+          //if user has not default category, craeting default
+          //category and moving records in to default category
+          const createDefaultCategory = await categoryService.createCategory(
+            "default",
+            userExsists
+          );
+
+          await recordService.moveRecordsToDefaultCategory(
+            records,
+            createDefaultCategory
+          );
+          return response.json(
+            `deleted ${userHasCategory.categoryName}, created default category and moved ${userHasCategory.categoryName} records in default `
+          );
+        }
+      } else {
+        return response.json("user doesnot have a category with this id");
+      }
+    } else {
+      throw new Error("user doesnt exsists or password is incorrect");
+    }
   } catch (err) {
     next(err);
   }
