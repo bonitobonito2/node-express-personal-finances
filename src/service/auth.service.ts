5a1040a5ec4071440e625cadabcb929f944444fc
bonitobonito2 <zaal.tsagareli.1@btu.edu.ge>
2023-03-18 04:01:14 +0400

    improve error handling

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,32 +1,45 @@
-import { getRepository } from "typeorm";
+import { Repository, getRepository } from "typeorm";
 import { User } from "../entities/user.entity";
 import { userInterface } from "../interfaces/user.interface";
+import { myDataSource } from "../database/db.config";
 
 export class AuthService {
-  public userRepo = getRepository(User);
+  public userRepo = myDataSource.getRepository(User);
 
   public async getUser(userName: string): Promise<User> {
-    return await this.userRepo.findOneBy({ username: userName });
+    try {
+      return await this.userRepo.findOneBy({ username: userName });
+    } catch (err) {
+      throw new Error(err);
+    }
   }
 
   public async createUser(userInfo: userInterface): Promise<boolean> {
-    const data = await this.userRepo.insert({
-      username: userInfo.userName,
-      password: userInfo.password,
-    });
+    try {
+      const data = await this.userRepo.insert({
+        username: userInfo.userName,
+        password: userInfo.password,
+      });
 
-    if (data) return true;
-    return false;
+      if (data) return true;
+      return false;
+    } catch (err) {
+      throw new Error(err);
+    }
   }
 
   public async changePassword(
     userName: string,
     password: string
   ): Promise<Boolean> {
-    const user = await this.getUser(userName);
+    try {
+      const user = await this.getUser(userName);
+      user.password = password;
 
-    user.password = password;
-    if (await this.userRepo.save(user)) return true;
-    return false;
+      if (await this.userRepo.save(user)) return true;
+      return false;
+    } catch (err) {
+      throw new Error(err);
+    }
   }
 }
