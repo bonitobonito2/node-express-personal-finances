8d4202c2f369260598ecc38fa054ffa103aa91a1
bonitobonito2 <zaal.tsagareli.1@btu.edu.ge>
2023-03-19 22:01:00 +0400

    get route on filter

--- src/routes/record.routes.ts
+++ src/routes/record.routes.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import { createRecordValidate } from "../requestValidations/record/createRecord.joy";
-import { createRecord } from "../controller/record/record.controller";
+import {
+  createRecord,
+  getFilteredRecord,
+} from "../controller/record/record.controller";
 
 const RecordRouter = express.Router();
 
 RecordRouter.post("/create", createRecordValidate, createRecord);
+
+RecordRouter.get("/filter", getFilteredRecord);
 
 export default RecordRouter;
