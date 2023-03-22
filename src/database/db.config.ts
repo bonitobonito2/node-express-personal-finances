be21bd3acfd4112e079192a2fa76cdf5f5ee710b
bonitobonito2 <zaal.tsagareli.1@btu.edu.ge>
2023-03-22 19:59:12 +0400

    added port env

--- src/database/db.config.ts
+++ src/database/db.config.ts
@@ -8,7 +8,7 @@ dotenv.config();
 export const myDataSource = new DataSource({
   type: "postgres",
   host: process.env.DATABASE_MASTER_HOST,
-  port: 5432,
+  port: parseInt(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
