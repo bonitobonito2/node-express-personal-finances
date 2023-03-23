b6785dcee59e0cf9d5cc5febd29eb1e260d406ca
bonitobonito2 <zaal.tsagareli.1@btu.edu.ge>
2023-03-23 14:11:27 +0400

    token validation middleware

---
+++ src/middlewares/validateToken.middleware.ts
@@ -0,0 +1,16 @@
+import { RequestHandler } from "express";
+import jwt from "jsonwebtoken";
+
+export const validateToken: RequestHandler = (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) {
+    throw new Error("u need token on protected routes");
+  }
+  try {
+    const decoded = jwt.verify(token, "topSecret21");
+    req["decoded"] = decoded;
+    next();
+  } catch (Err) {
+    return res.status(404).send(Err);
+  }
+};
