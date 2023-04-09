18727c6807fc81e2ed76ef22cd1fc6f9095381be
bonitobonito2 <zaal.tsagareli.1@btu.edu.ge>
2023-04-09 20:49:59 +0400

    db config better

--- src/database/db.config.ts
+++ src/database/db.config.ts
@@ -8,10 +8,17 @@ dotenv.config();
 export const myDataSource = new DataSource({
   type: "postgres",
   host: process.env.DATABASE_MASTER_HOST,
-  port: parseInt(process.env.DATABASE_PORT),
+  port: parseInt(process.env.DATABASE_PORT), // or the port number you are using
   username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
+  // password: "QsS3jlkdyCR5",
   database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
   entities: [User, Category, Records],
   synchronize: true,
+  extra: {
+    ssl: true,
+    sslmode: "require",
+  },
 });
+
+// postgres://bonitobonito2:QsS3jlkdyCR5@ep-wispy-term-125918.eu-central-1.aws.neon.tech/neondb
