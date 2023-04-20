71d629cd625150be1c414c3dc80334c7c3fcb7be
bonitobonito2 <zaaltsagareli@gmail.com>
2023-04-21 03:27:28 +0400

    tt

--- src/middlewares/validateToken.middleware.ts
+++ src/middlewares/validateToken.middleware.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from "express";
 import jwt from "jsonwebtoken";
 
 export const validateToken: RequestHandler = (req, res, next) => {
+  console.log("testt ", req.url);
   const token = req.header("Authorization")?.replace("Bearer ", "");
   if (!token) {
     throw new Error("u need token on protected routes");
